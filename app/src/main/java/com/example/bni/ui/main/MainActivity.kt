850a7093691d989977f5c913ed4523bcb55c1d46
Muhammad Aqil <haimac@Aqils-MacBook-Pro.local>
2024-08-12 23:03:36 +0700

    add main activity

---
+++ app/src/main/java/com/example/bni/ui/main/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.bni.ui.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.bni.ui.theme.BniTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BniTheme {
+
+            }
+        }
+    }
+}
