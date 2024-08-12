bea5d563ccb22cf2676b350b7f01654c529be4a4
Muhammad Aqil <haimac@Aqils-MacBook-Pro.local>
2024-08-12 23:17:51 +0700

    add hilt annotation in the base application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt)
     id(libs.plugins.kotlin.plugin.serialization.get().pluginId)
     kotlin(libs.plugins.kapt.get().pluginId)
 }
