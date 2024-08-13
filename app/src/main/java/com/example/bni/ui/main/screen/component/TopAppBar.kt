84a536d9f0094440fee3bf35ffcea1e29c5a0c47
Muhammad Aqil <haimac@Aqils-MacBook-Pro.local>
2024-08-13 22:53:13 +0700

    create top app bar components

---
+++ app/src/main/java/com/example/bni/ui/main/screen/component/TopAppBar.kt
@@ -0,0 +1,42 @@
+package com.example.bni.ui.main.screen.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(
+    canNavigateBack: Boolean,
+    title: String,
+    onFinish: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(text = title, style = MaterialTheme.typography.titleMedium)
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Transparent,
+            navigationIconContentColor = Color.Unspecified,
+        ),
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = {
+                    onFinish()
+                }) {
+                    Image(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Arrow Back"
+                    )
+                }
+            }
+        })
+}
