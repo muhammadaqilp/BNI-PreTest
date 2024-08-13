1adbe4dddd3a35a9b6f6515793e18c83c85b23f6
Muhammad Aqil <haimac@Aqils-MacBook-Pro.local>
2024-08-13 16:54:17 +0700

    add main activity

--- app/src/main/java/com/example/bni/ui/MainActivity.kt
+++ app/src/main/java/com/example/bni/ui/MainActivity.kt
@@ -1,18 +1,20 @@
-package com.example.bni.ui.main
+package com.example.bni.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.bni.ui.theme.BniTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BniTheme {
-
+                QrPaymentApp()
             }
         }
     }
