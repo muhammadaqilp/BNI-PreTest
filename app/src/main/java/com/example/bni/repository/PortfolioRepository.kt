edc590d24c597b882319175881241ee90291e486
Muhammad Aqil <haimac@Aqils-MacBook-Pro.local>
2024-08-14 23:10:39 +0700

    enhance file

--- app/src/main/java/com/example/bni/repository/PortfolioRepository.kt
+++ app/src/main/java/com/example/bni/repository/PortfolioRepository.kt
@@ -3,15 +3,17 @@ package com.example.bni.repository
 import com.example.bni.data.response.PortfolioDataResponse
 import com.example.bni.model.PortfolioModel
 import com.example.bni.utils.AppUtils.getJsonString
-import com.google.gson.Gson
+import com.example.bni.utils.ChartDataAdapterFactory
+import com.google.gson.GsonBuilder
 import javax.inject.Inject
 
-class PortfolioRepository @Inject constructor(
-    private val gson: Gson
-) : IPortfolioRepository {
+class PortfolioRepository @Inject constructor(): IPortfolioRepository {
 
     override fun getPortfolioData(): PortfolioModel {
-        val response = gson.fromJson(
+        val gs = GsonBuilder()
+            .registerTypeAdapterFactory(ChartDataAdapterFactory())
+            .create()
+        val response = gs.fromJson(
             getJsonString("portfolio_response.json"), Array<PortfolioDataResponse>::class.java
         ).toList()
         return PortfolioModel.mapper(response)
